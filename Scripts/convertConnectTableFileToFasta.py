a0d1fdd8102e0fc61e8d22caa39977612760ce4c
Stefan Redl <bernhardlaus@gmx.at>
2025-02-18 22:56:03 +0100

    rename Documents;

--- Scripts/convertConnectTableFileToFasta.py
+++ Scripts/convertConnectTableFileToFasta.py
@@ -4,7 +4,7 @@ def ctToFasta(ct_file, fasta_file, sequence_name="B_subtilis"):
         
         sequence = "".join(line.split()[1] for line in lines[1:])
         
-        fasta.write(f">{sequence_name}\n")
+        fasta.write(f">{sequence_name}\n") 
         fasta.write(sequence + "\n")
 
 ctToFasta("D:\Masterarbeit_programmieren\Data\B.subtilis.ct2", "D:\Masterarbeit_programmieren\Data\B.subtilis.fasta")
