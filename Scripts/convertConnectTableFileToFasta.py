227b43bef7ef807f2acceb2e7dbc661142153502
Stefan Redl <bernhardlaus@gmx.at>
2025-02-18 22:45:27 +0100

    rename Docus; convert the ct2 file into fasta

---
+++ Scripts/convertConnectTableFileToFasta.py
@@ -0,0 +1,10 @@
+def ctToFasta(ct_file, fasta_file, sequence_name="B_subtilis"):
+    with open(ct_file, "r") as ct, open(fasta_file, "w") as fasta:
+        lines = ct.readlines()
+        
+        sequence = "".join(line.split()[1] for line in lines[1:])
+        
+        fasta.write(f">{sequence_name}\n")
+        fasta.write(sequence + "\n")
+
+ctToFasta("D:\Masterarbeit_programmieren\Data\B.subtilis.ct2", "D:\Masterarbeit_programmieren\Data\B.subtilis.fasta")
