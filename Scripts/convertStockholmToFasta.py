7ada028328e24295be51af6544f45853ed025be6
Stefan Redl <stefan.redl@isis-papyrus.com>
2025-02-19 07:23:17 +0100

    fix issue with convert stockholm files to fasta files; download sequences from RFAM (don't now wheter there right)

--- Scripts/convertStockholmToFasta.py
+++ Scripts/convertStockholmToFasta.py
@@ -2,18 +2,18 @@ import os
 from Bio import AlignIO
 from Bio import SeqIO
 
-SAMPLES_STOCKHOLM = "/mnt/d/Masterarbeit/Scripts/Data/"
-SAMPLES_FASTA = "/mnt/d/Masterarbeit/Scripts/Data/"
+SAMPLES_STOCKHOLM = "C:/bla/Waste/MA/Data/"
+SAMPLES_FASTA = "C:/bla/Waste/MA/Data/"
 
 def convertStockholmToFasta(inputDir):
     for file in os.listdir(inputDir):
-        if file.endswith(".clu"):
+        if file.endswith(".stockholm"):
             input_file_path = os.path.join(inputDir, file)
             output_file_path = os.path.join(SAMPLES_FASTA, os.path.splitext(file)[0] + ".fasta")
             
             try:
-                alignment = AlignIO.read(input_file_path, "stockholm")
-                count = AlignIO.write(alignment, output_file_path, "stockholm")
+                alignment = SeqIO.parse(input_file_path, "stockholm")
+                count = SeqIO.write(alignment, output_file_path, "fasta")
                 print(f"Converted {count} records from {file} to {output_file_path}")
             except Exception as e:
                 print(f"Error processing file {file}: {e}")
