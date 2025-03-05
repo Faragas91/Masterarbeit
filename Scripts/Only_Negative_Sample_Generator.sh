517d96da6f2292d93f6325b92cbb1d02dcb9f497
Stefan Redl <stefan.redl@isis-papyrus.com>
2025-03-05 07:54:18 +0100

    configure script to generate negative Samples from the native alignments

--- Scripts/Only_Negative_Sample_Generator.sh
+++ Scripts/Only_Negative_Sample_Generator.sh
@@ -6,15 +6,16 @@ TOTAL_FILES=$((500000))  # Total number of files to be processed
 SISSIz="/home/sredl/Masterarbeit/tools/sissiz_v3/src/SISSIz"
 ALIFOLDZ="/home/sredl/Masterarbeit/tools/shuffle-aln.pl"  
 MULTIPERM="/home/sredl/Masterarbeit/tools/multiperm-0.9.4/multiperm" 
-NEGATIVE_SAMPLES="/home/sredl/Masterarbeit/NEGATIVE_SAMPLES"  
-NATIVE_SAMPLES="/home/sredl/Masterarbeit/native_samples.clu"
+NATIVE_SAMPLES="/home/sredl/Masterarbeit/Data/NATIVE_SAMPLES/"
+NEGATIVE_SAMPLES="/home/sredl/Masterarbeit/Data/NEGATIVE_SAMPLES/"
+POSITIVE_SAMPLES="$NATIVE_SAMPLES/bacillus_subtilis_Rfam.clu"
 
 # Make sure the executables have the necessary permissions
 chmod +x "$SISSIz"
 chmod +x "$MULTIPERM"
 
-# Create the output directories if they don't exist
-mkdir -p "$NEGATIVE_SAMPLES"  
+mkdir -p "$NATIVE_SAMPLES"
+mkdir -p "$NEGATIVE_SAMPLES"
 
 current_file=0
 
@@ -24,8 +25,6 @@ function show_progress {
     echo -ne "Progress: $percentage% ($current_file/$TOTAL_FILES) completed\r"
 }
 
-for i in "$NATIVE_SAMPLES"; do
-
 #################################
 ########## SISSIz_mono ##########
 #################################
@@ -33,11 +32,11 @@ for i in "$NATIVE_SAMPLES"; do
     # Loop to generate 100000 SISSIz mononucleotide samples for each SISSI sample
     for z in $(seq 1 $ITER_NEG); do
         file_step=0
-        SISSIz_mono_OUTPUT="$NEGATIVE_SAMPLES/neg_sample_SISSIz_mono_output_${i}_${z}.clu"
+        SISSIz_mono_OUTPUT="$NEGATIVE_SAMPLES/neg_sample_SISSIz_mono_output_${z}.clu"
 
         if [ ! -f "$SISSIz_mono_OUTPUT" ]; then
             # Run the SISSIz_mono command
-            "$SISSIz" -s -i $SISSI_OUTPUT > $SISSIz_mono_OUTPUT
+            "$SISSIz" -s -i $POSITIVE_SAMPLES > $SISSIz_mono_OUTPUT
             echo "$SISSIz_mono_OUTPUT finished" 
         else 
             echo "$SISSIz_mono_OUTPUT already exists, skipping..."
@@ -60,11 +59,11 @@ for i in "$NATIVE_SAMPLES"; do
     # Loop to generate 100000 SISSIz dinucleotide samples for each SISSI sample
     for v in $(seq 1 $ITER_NEG); do
         file_step=0
-        SISSIz_di_OUTPUT="$NEGATIVE_SAMPLES/neg_sample_SISSIz_di_output_${i}_${v}.clu"
+        SISSIz_di_OUTPUT="$NEGATIVE_SAMPLES/neg_sample_SISSIz_di_output_${v}.clu"
 
         if [ ! -f "$SISSIz_di_OUTPUT" ]; then
             # Run the SISSIz_di command
-            "$SISSIz" -s $SISSI_OUTPUT > $SISSIz_di_OUTPUT
+            "$SISSIz" -s $POSITIVE_SAMPLES > $SISSIz_di_OUTPUT
             echo "$SISSIz_di_OUTPUT finished"
         else 
             echo "$SISSIz_di_OUTPUT already exists, skipping..."
@@ -88,11 +87,11 @@ for i in "$NATIVE_SAMPLES"; do
     # Loop to generate Multiperm mononucleotide samples
     for m in $(seq 1 $ITER_NEG); do
         file_step=0
-        MULTIPERM_mono_OUTPUT="$NEGATIVE_SAMPLES/neg_sample_MULTIPERM_mono_output_${i}_${m}.clu"
+        MULTIPERM_mono_OUTPUT="$NEGATIVE_SAMPLES/neg_sample_MULTIPERM_mono_output_${m}.clu"
 
         if [ ! -f "$MULTIPERM_mono_OUTPUT" ]; then
             # Run Multiperm command
-            "$MULTIPERM" -w --conservation=none "$SISSI_OUTPUT" && mv perm_001_pos_sample_*.clu "$MULTIPERM_mono_OUTPUT"
+            "$MULTIPERM" -w --conservation=none "$POSITIVE_SAMPLES" && mv perm_001_pos_sample_*.clu "$MULTIPERM_mono_OUTPUT"
             echo "$MULTIPERM_mono_OUTPUT finished"
         else 
             echo "$MULTIPERM_mono_OUTPUT already exists, skipping..."
@@ -115,11 +114,11 @@ for i in "$NATIVE_SAMPLES"; do
     # Loop to generate Multiperm dinucleotide samples
     for j in $(seq 1 $ITER_NEG); do
         file_step=0
-        MULTIPERM_di_OUTPUT="$NEGATIVE_SAMPLES/neg_sample_MULTIPERM_di_output_${i}_${j}.clu"
+        MULTIPERM_di_OUTPUT="$NEGATIVE_SAMPLES/neg_sample_MULTIPERM_di_output_${j}.clu"
 
         if [ ! -f "$MULTIPERM_di_OUTPUT" ]; then
             # Run Multiperm command
-            "$MULTIPERM" -w "$SISSI_OUTPUT" && mv perm_001_pos_sample_*.clu "$MULTIPERM_di_OUTPUT"
+            "$MULTIPERM" -w "$POSITIVE_SAMPLES" && mv perm_001_pos_sample_*.clu "$MULTIPERM_di_OUTPUT"
             echo "$MULTIPERM_di_OUTPUT finished"
         else 
             echo "$MULTIPERM_di_OUTPUT already exists, skipping..."
@@ -142,11 +141,11 @@ for i in "$NATIVE_SAMPLES"; do
     # Loop to generate Alifoldz samples
     for k in $(seq 1 $ITER_NEG); do
         file_step=0
-        ALIFOLDz_OUTPUT="$NEGATIVE_SAMPLES/neg_sample_ALIFOLDz_output_${i}_${k}.clu"
+        ALIFOLDz_OUTPUT="$NEGATIVE_SAMPLES/neg_sample_ALIFOLDz_output_${k}.clu"
             
         if [ ! -f "$ALIFOLDz_OUTPUT" ]; then
             # Run Alifoldz command
-            perl "$ALIFOLDZ" < "$SISSI_OUTPUT" > "$ALIFOLDz_OUTPUT"
+            perl "$ALIFOLDZ" < "$POSITIVE_SAMPLES" > "$ALIFOLDz_OUTPUT"
             echo "$ALIFOLDz_OUTPUT finished"
         else 
             echo "$ALIFOLDz_OUTPUT already exists, skipping..."
@@ -161,5 +160,4 @@ for i in "$NATIVE_SAMPLES"; do
             file_step=0
         fi
     done
-
 echo -ne "\nProcessing completed.\n"
