7c69d5974642f89ca8d371cc679b521302d5d265
Stefan Redl <bernhardlaus@gmx.at>
2025-04-17 22:27:21 +0200

    bla

--- 2.Versuch/Scripts/converterClustalToFasta.py
+++ 2.Versuch/Scripts/converterClustalToFasta.py
@@ -1,8 +1,10 @@
 import os
 from Bio import SeqIO
 
-SAMPLES_CLUSTAL = "D:/Masterarbeit_programmieren/2.Versuch/Data/SAMPLES/"
-SAMPLES_FASTA = "D:/Masterarbeit_programmieren/2.Versuch/Data/SAMPLES_FASTA/"
+# SAMPLES_CLUSTAL = "D:/Masterarbeit_programmieren/2.Versuch/Data/SAMPLES_CLUSTAL/"
+SAMPLES_CLUSTAL = "/home/sredl/Masterarbeit/2.Versuch/Data/SAMPLES_CLUSTAL/"
+#SAMPLES_FASTA = "D:/Masterarbeit_programmieren/2.Versuch/Data/SAMPLES_FASTA/"
+SAMPLES_FASTA = "/home/sredl/Masterarbeit/2.Versuch/Data/SAMPLES_FASTA/"
 
 def convertClustalToFasta(inputDir):
     for file in os.listdir(inputDir):
--- 2.Versuch/Scripts/transferMXfold2DataToExcel.py
+++ 2.Versuch/Scripts/transferMXfold2DataToExcel.py
@@ -1,15 +1,20 @@
 import os
 import shutil
 import pandas as pd
+import statistics
 
 # Function to parse the RNAz file
 def parse_mxfold2_file(file_path):
-    # Define the keys for the relevant columns (starting with column 4)
+    number = []
     with open(file_path, 'r') as file:
-        first_line = file.readline().strip().split()
-
-    return first_line
-
+        for line in file:
+            line_row = line.strip().split()
+            if len(line_row) > 1:
+                value = float(line_row[1].replace("(", "").replace(")", ""))
+                number.append(value)
+        print(number)
+        result_number = statistics.median(number)
+        return result_number
 
 # All data
 def createExcelData(data, count, nameOfFile, excelName):
@@ -18,24 +23,39 @@ def createExcelData(data, count, nameOfFile, excelName):
             count += 1
             print(f"Process file {count}: {file_name}")
             file_path = os.path.join(directory, file_name)
-            file_data = parse_mxfold2_file(file_path)
-            file_data["File"] = file_name  
-            data.append(file_data)
+            file_data = parse_mxfold2_file(file_path)  
+            data.append({"Score": file_data, "File": file_name})
 
     df = pd.DataFrame(data)
     df.to_excel(f"{excelName}.xlsx", index=False)
 
     print(f"Your data was succsessfully transfered to {excelName}.xlsx.")
-    shutil.move(f"C:/bla/Waste/MA/{excelName}.xlsx", f"C:/bla/Waste/MA/2.Versuch/Data/MXfold2_Excel/{excelName}.xlsx")
+    shutil.move(f"D:/Masterarbeit_programmieren/{excelName}.xlsx", f"D:/Masterarbeit_programmieren/2.Versuch/Data/MXfold2_Excel/{excelName}.xlsx") #f"C:/bla/Waste/MA/{excelName}.xlsx", f"C:/bla/Waste/MA/2.Versuch/Data/MXfold2_Excel/{excelName}.xlsx")
     return count
 
-#directory = "D:/Masterarbeit_programmieren/2.Versuch/Data/MXfold2_SAMPLES/"
-directory = "C:/bla/Waste/MA/2.Versuch/Data/MXfold2_SAMPLES"
+directory = "D:/Masterarbeit_programmieren/2.Versuch/Data/MXfold2_SAMPLES/"
+#directory = "C:/bla/Waste/MA/2.Versuch/Data/MXfold2_SAMPLES"
+
+count = 0
+sissi_pos_data = []
+count = createExcelData(sissi_pos_data, count, "pos_sample", "sissi")
 
 count = 0
 alifoldz_data = []
 count = createExcelData(alifoldz_data, count, "neg_sample_ALIFOLDz", "alifoldz")
 
 count = 0
-sissi_pos_data = []
-count = createExcelData(sissi_pos_data, count, "pos_sample", "sissi")
+multiperm_mono_data = []
+count = createExcelData(multiperm_mono_data, count, "neg_sample_MULTIPERM_mono", "multiperm_mono")
+
+count = 0
+multiperm_di_data = []
+count = createExcelData(multiperm_di_data, count, "neg_sample_MULTIPERM_di", "multiperm_di")
+
+count = 0
+sissiz_mono_data = []
+count = createExcelData(sissiz_mono_data, count, "neg_sample_SISSIz_mono", "sissiz_mono")
+
+count = 0
+sissiz_di_data = []
+count = createExcelData(sissiz_di_data, count, "neg_sample_SISSIz_di", "sissiz_di")
