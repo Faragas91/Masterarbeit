a7a46d06ffdbdb5654385942de569212f237830d
Stefan Redl <stefan.redl@isis-papyrus.com>
2025-06-06 11:54:18 +0200

    test

--- 2.Versuch/Scripts/converterFastaToUFoldInputStructure.py
+++ 2.Versuch/Scripts/converterFastaToUFoldInputStructure.py
@@ -10,23 +10,22 @@ if not os.path.exists(SAMPLES_UFOLD):
 
 def convertFastaToUfoldStructure(inputDir):
     for file in os.listdir(inputDir):
-        lineBreak = False
-        with open(INPUT_FILE, 'w') as input_file:
-            pass
         file_path = os.path.join(inputDir, file)
         with open(file_path) as lines:
+            sequenceCount = 0
+            current_sequence = []
             for line in lines:
                 if line.startswith(">"):
-                    with open(INPUT_FILE, 'a') as input_file:
-                        if lineBreak:
-                           input_file.write('\n' + line.strip() + '\n') 
-                        else:
-                            input_file.write(line.strip() + '\n')
-                if line.startswith(("A", "T", "G", "C", "U")):
-                    with open(INPUT_FILE, 'a') as input_file:
-                        input_file.write(line.strip())
-                    lineBreak = True
-        os.rename(INPUT_FILE, file)
-        shutil.move(file, SAMPLES_UFOLD)
+                    if current_sequence:
+                        with open(os.path.join(SAMPLES_UFOLD, f'seq{sequenceCount}_{file}'), 'w') as output_file:
+                            output_file.write(''.join(current_sequence))
+                        sequenceCount += 1
+                        current_sequence = [] 
+                    current_sequence.append(line.strip() + '\n')
+                else:
+                    current_sequence.append(line.strip())
+            if current_sequence:
+                with open(os.path.join(SAMPLES_UFOLD, f'seq{sequenceCount}_{file}'), 'w') as output_file:
+                    output_file.write(''.join(current_sequence))
 
 convertFastaToUfoldStructure(SAMPLES_FASTA)
