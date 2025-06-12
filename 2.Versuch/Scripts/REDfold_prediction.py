33e4aa68d1dd2874e7c2ded2facdb798d492a9a5
Stefan Redl <bernhardlaus@gmx.at>
2025-06-12 21:16:27 +0200

    generate prediction output of REDfold

--- 2.Versuch/Scripts/REDfold_prediction.py
+++ 2.Versuch/Scripts/REDfold_prediction.py
@@ -1,25 +1,25 @@
-import subprocess
-from multiprocessing import Pool
 import os
 import time
+import subprocess
+from tqdm import tqdm
 
-SAMPLES = "/mnt/sdc2/home/c2210542009/Masterarbeit/Data/SAMPLES_FASTA"
-REDFOLD_PRE_OUTPUT = "/mnt/sdc2/home/c2210542009/Masterarbeit/Data/REDFOLD_PREDICTION"
+SAMPLES_REDFOLD = "/mnt/sdc2/home/c2210542009/Masterarbeit/Data/TEST_SAMPLES/SAMPLES_REDFOLD"
+REDFOLD_PRE_OUTPUT = "/mnt/sdc2/home/c2210542009/Masterarbeit/Data/TEST_SAMPLES/REDFOLD_PREDICTION"
 
 os.makedirs(REDFOLD_PRE_OUTPUT, exist_ok=True)
-samples = ['/TEST_SAMPLES1', '/TEST_SAMPLES2', '/TEST_SAMPLES3', '/TEST_SAMPLES4']
 
 start_time = time.time()
 print(f"Started at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
 
-def run_redfold(sample):
-    log_file_path = os.path.join(REDFOLD_PRE_OUTPUT, f"{os.path.basename(sample)}.log")
-    with open(log_file_path, 'w') as log_file:
-        subprocess.run(['redfold', sample], stdout=log_file, stderr=subprocess.STDOUT)
+def run_redfold():
+    for folder_name in tqdm(['ALIFOLDz', 'MULTIPERM_MONO', 'MULTIPERM_DI', 'SISSIz_MONO', 'SISSIz_DI', 'POS_SAMPLES'], desc="REDfold"):
+        sample_path = os.path.join(SAMPLES_REDFOLD, folder_name)
+        log_path = os.path.join(REDFOLD_PRE_OUTPUT, f"{folder_name}.txt")
+        
+        with open(log_path, 'w') as log_file:
+            subprocess.run(['redfold', sample_path], stdout=log_file, stderr=subprocess.STDOUT)
 
-if __name__ == '__main__':
-    with Pool(processes=len(samples)) as pool:
-        pool.map(run_redfold, samples)
+run_redfold()
 
 end_time = time.time()
 print(f"Finished at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}")
--- 2.Versuch/Scripts/convertMSAToSF.py
+++ 2.Versuch/Scripts/convertMSAToSF.py
@@ -2,37 +2,42 @@ import os
 from os import path
 import shutil
 
-# SAMPLES_FASTA = "/mnt/sdc2/home/c2210542009/Masterarbeit/Data/TEST_SAMPLES/SAMPLES_FASTA/"
-# SAMPLES_REDFOLD = "/mnt/sdc2/home/c2210542009/Masterarbeit/Data/TEST_SAMPLES/SAMPLES_REDFOLD/"
-SAMPLES_REDFOLD = "C:/bla/Waste/MA/2.Versuch/Data/SAMPLES_REDFOLD/"
+SAMPLES_FASTA = "/mnt/sdc2/home/c2210542009/Masterarbeit/Data/TEST_SAMPLES/SAMPLES_FASTA/"
+SAMPLES_REDFOLD = "/mnt/sdc2/home/c2210542009/Masterarbeit/Data/TEST_SAMPLES/SAMPLES_REDFOLD/"
 METHODE = ["ALIFOLDz", "MULTIPERM_MONO", "MULTIPERM_DI", "SISSIz_MONO", "SISSIz_DI", "POS_SAMPLES"]
 
 for i in METHODE:
     if not os.path.exists(os.path.join(SAMPLES_REDFOLD, i)):
         os.makedirs(os.path.join(SAMPLES_REDFOLD, i))
 
-# if not os.path.exists(SAMPLES_REDFOLD):
-#     os.makedirs(SAMPLES_REDFOLD)
+if not os.path.exists(SAMPLES_REDFOLD):
+    os.makedirs(SAMPLES_REDFOLD)
 
-# def convertFastaToUfoldStructure(inputDir):
-#     for file in os.listdir(inputDir):
-#         file_path = os.path.join(inputDir, file)
-#         with open(file_path) as lines:
-#             sequenceCount = 0
-#             current_sequence = []
-#             for line in lines:
-#                 if line.startswith(">"):
-#                     if current_sequence:
-#                         with open(os.path.join(SAMPLES_REDFOLD, f'seq{sequenceCount}_{file}'), 'w') as output_file:
-#                             output_file.write("".join(current_sequence).replace("T", "U"))
-#                         sequenceCount += 1
-#                         current_sequence = [] 
-#                     current_sequence.append(line.strip() + '/n')
-#                 else:
-#                     current_sequence.append(line.strip())
-#             if current_sequence:
-#                 with open(os.path.join(SAMPLES_REDFOLD, f'seq{sequenceCount}_{file}'), 'w') as output_file:
-#                     output_file.write(''.join(current_sequence))
+def convertFastaToUfoldStructure(inputDir):
+    for file in os.listdir(inputDir):
+        file_path = os.path.join(inputDir, file)
+        with open(file_path) as lines:
+            sequenceCount = 0
+            current_header = ""
+            current_sequence = []
+            for line in lines:
+                if line.startswith(">"):
+                    if current_sequence:
+                        out_file = os.path.join(SAMPLES_REDFOLD, f'seq{sequenceCount}_{file}')
+                        with open(out_file, 'w') as output_file:
+                            output_file.write(current_header + '\n')
+                            output_file.write("".join(current_sequence).replace("T", "U") + '\n')  # <- WICHTIG!
+                        sequenceCount += 1
+                        current_sequence = []
+                    current_header = line.strip()
+                else:
+                    current_sequence.append(line.strip())
+            if current_sequence:
+                out_file = os.path.join(SAMPLES_REDFOLD, f'seq{sequenceCount}_{file}')
+                with open(out_file, 'w') as output_file:
+                    output_file.write(current_header + '\n')
+                    output_file.write("".join(current_sequence).replace("T", "U") + '\n')  # <- WICHTIG!
+
 
 def splitFastaFiles(inputDir):
     for file in os.listdir(inputDir):
@@ -42,18 +47,20 @@ def splitFastaFiles(inputDir):
             
             if parts[3] == "ALIFOLDz":
                 shutil.move(os.path.join(SAMPLES_REDFOLD, file), os.path.join(SAMPLES_REDFOLD, METHODE[0]))
-            if parts[3] == "MULTIPERM_MONO":
-                shutil.move(os.path.join(SAMPLES_REDFOLD, file), os.path.join(SAMPLES_REDFOLD, METHODE[1]))
-            if parts[3] == "MULTIPERM_DI":
-                shutil.move(os.path.join(SAMPLES_REDFOLD, file), os.path.join(SAMPLES_REDFOLD, METHODE[2]))
-            if parts[3] == "SISSIz_MONO":
-                shutil.move(os.path.join(SAMPLES_REDFOLD, file), os.path.join(SAMPLES_REDFOLD, METHODE[3]))
-            if parts[3] == "SISSIz_DI":
-                shutil.move(os.path.join(SAMPLES_REDFOLD, file), os.path.join(SAMPLES_REDFOLD, METHODE[4]))
+            elif parts[3] == "MULTIPERM":
+                if parts[4] == "mono":
+                    shutil.move(os.path.join(SAMPLES_REDFOLD, file), os.path.join(SAMPLES_REDFOLD, METHODE[1]))
+                if parts[4] == "di":
+                    shutil.move(os.path.join(SAMPLES_REDFOLD, file), os.path.join(SAMPLES_REDFOLD, METHODE[2]))
+            elif parts[3] == "SISSIz":
+                if parts[4] == "mono":
+                    shutil.move(os.path.join(SAMPLES_REDFOLD, file), os.path.join(SAMPLES_REDFOLD, METHODE[3]))
+                if parts[4] == "di":
+                    shutil.move(os.path.join(SAMPLES_REDFOLD, file), os.path.join(SAMPLES_REDFOLD, METHODE[4]))
             else:
                 shutil.move(os.path.join(SAMPLES_REDFOLD, file), os.path.join(SAMPLES_REDFOLD, METHODE[5]))
 
-# convertFastaToUfoldStructure(SAMPLES_FASTA)
+convertFastaToUfoldStructure(SAMPLES_FASTA)
 splitFastaFiles(SAMPLES_REDFOLD)
 
 
