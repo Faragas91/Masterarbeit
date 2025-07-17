14fc0a4d93ee413696d1f459172b4a0d9c0cfab0
Stefan Redl <stefan.redl@isis-papyrus.com>
2025-07-17 07:41:25 +0200

    finished script to split ClustalW files

--- 2.Versuch/Scripts/for_Native_Data/splitClustalWFilesInSubfiles.py
+++ 2.Versuch/Scripts/for_Native_Data/splitClustalWFilesInSubfiles.py
@@ -1,18 +1,22 @@
 import sys, os
 from Bio import SeqIO
 
-input_file = sys.argv[1]
-step = int(sys.argv[2])
-out_prefix = sys.argv[3]
+input_path = "C:/bla/Waste/MA/2.Versuch/Data/SAMPLES_CLUSTAL"
+for j in os.listdir(input_path):
+    input_file = j.split("/")[-1]
+    file_name = input_file.split(".")[0]
+    step = 6
+    full_path = os.path.join(input_path, input_file)
+    out_prefix = f"C:/bla/Waste/MA/2.Versuch/Data/ClustalW_Subfiles/{file_name}"
 
-records = list(SeqIO.parse(input_file, "clustal"))
-if len(records) == 0:
-    sys.exit("No Sequence found")
+    records = list(SeqIO.parse(full_path, "clustal"))
+    if len(records) == 0:
+        sys.exit("No Sequence found")
 
-for i in range(0, len(records), step):
-    chunk = records[i:i+step]
-    idx = i//step + 1
-    out = f"{out_prefix}_{idx:02d}.clu"
-    with open(out, "w") as handle:
-        SeqIO.write(chunk, handle, "clustal")
-        print(f"Write {out} with {len(chunk)} Sequences")
+    for i in range(0, len(records), step):
+        chunk = records[i:i+step]
+        idx = i//step + 1
+        out = f"{out_prefix}_{idx:02d}.clu"
+        with open(out, "w") as handle:
+            SeqIO.write(chunk, handle, "clustal")
+            print(f"Write {out} with {len(chunk)} Sequences")
